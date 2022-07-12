6993a4f846e0385b691a711078f8a1303f8c2c22
Becky Nisttahuz <benistta@hotmail.com>
2022-07-12 13:12:56 -0600

    Feat: update request test for merchant

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,24 +1,24 @@
-# require 'rails_helper'
-#
-# RSpec.describe "Mercahnts API" do
-#   it 'sends a list of merchants' do
-#     create_list(:merchant, 3)
-#
-#     get '/api/v1/merchants'
-#
-#     expect(response).to be_successful
-#     merchants = JSON.parse(response.body, symbolize_names: true)
-#
-#     expect(merchants).to be_an(Array)
-#     expect(merchants[:data].count).to eq(3)
-#
-#     merchants[:data].each do |merchant|
-#
-#       expect(merchant).to have_key(:id)
-#       expect(merchant[:id].to_i).to be_an(Integer)
-#
-#       expect(merchant[:attributes]).to have_key(:name)
-#       expect(merchant[:attributes][:name]).to be_a(String)
-#     end
-#   end
-# end
+require 'rails_helper'
+
+RSpec.describe "Mercahnts API" do
+  it 'sends a list of merchants' do
+    create_list(:merchant, 3)
+
+    get '/api/v1/merchants'
+
+    expect(response).to be_successful
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants).to be_an(Array)
+    expect(merchants[:data].count).to eq(3)
+
+    merchants[:data].each do |merchant|
+
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id].to_i).to be_an(Integer)
+
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_a(String)
+    end
+  end
+end
