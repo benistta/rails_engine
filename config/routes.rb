0e774e7311eb2ead81f2e6746b91a6707e7a9f1d
Becky Nisttahuz <benistta@hotmail.com>
2022-07-12 14:59:19 -0600

    Update: routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      # namespace :merchants do
-        resources :merchants, only: [:index, :show]
+      namespace :merchants do
+        # resources :merchants, only: [:index, :show]
       end
-      # resources :merchants, only: [:index, :show]
+      resources :merchants, only: [:index, :show] do
+      # get 'items', to: 'merchants/items#index'
+      resources :items, controller: 'merchant_items', only: [:index]
+      end
     end
   end
-# end
+end
