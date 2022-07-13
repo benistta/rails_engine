a1296cf8a2dbbf1521102ae45f05e0b893dfc49d
Becky Nisttahuz <benistta@hotmail.com>
2022-07-12 18:02:33 -0600

    Update: item factories

--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -4,6 +4,5 @@ FactoryBot.define do
     name { Faker::Commerce.product_name }
     description {Faker::Lorem.sentence}
     unit_price { Faker::Number.within(range: 1..1000) }
-    #merchant_id {Faker::Number.within(range: 1..100)}
   end
 end
