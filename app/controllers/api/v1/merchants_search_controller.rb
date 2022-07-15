0476be8886c74fa85808955cd9b4fa7ec993f6dc
Becky Nisttahuz <benistta@hotmail.com>
2022-07-14 21:48:34 -0600

    Update:Add find and find all method in merchant search controller

--- app/controllers/api/v1/merchants_search_controller.rb
+++ app/controllers/api/v1/merchants_search_controller.rb
@@ -1,13 +1,20 @@
 class Api::V1::MerchantsSearchController < ApplicationController
-
+  
   def find
-
+    if params[:name].present?
     merchant = Merchant.find_merchant(params[:name])
-    if merchant.nil?
-      render json: { data: { :message => 'merchant not found'} }
-      # render json: { data: {} }
-    else
-      render json: MerchantSerializer.new(merchant)
+      if merchant
+        render json: MerchantSerializer.new(merchant)
+      elsif merchant.nil?
+        render json: { data: { message: 'Error: Merchant Not Found' } }
+      end
+    elsif params[:name].empty?
+      render status: 400
     end
+  end
+
+  def find_all
+    merchants = Merchant.find_all_merchants(params[:name])
+    render json: MerchantSerializer.new(merchants) if merchants
   end
 end
