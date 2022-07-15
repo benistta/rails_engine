3659922b268722f5b8eeff40db4a0cbb04ec97ff
Becky Nisttahuz <benistta@hotmail.com>
2022-07-14 21:49:13 -0600

    Refactor: show method in merchant controller

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,14 +1,11 @@
 class Api::V1::MerchantsController < ApplicationController
+  
   def index
-    # render json: MerchantSerializer.new(Merchant.all)
     render(json: MerchantSerializer.new(Merchant.all), statu: :ok)
-    # render json: MerchantSerializer.format_index(Merchant.all)
   end
 
   def show
     merchant = Merchant.find(params[:id])
-    # render json: MerchantSerializer.format_show(merchant)
-    # render json: MerchantSerializer.new(merchant)
     render(json: MerchantSerializer.new(merchant), status: :ok)
   end
 end
