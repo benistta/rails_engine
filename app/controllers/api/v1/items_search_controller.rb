dff623355189f849869b8cbcc3d97c067bfc67a1
Becky Nisttahuz <benistta@hotmail.com>
2022-07-14 21:50:00 -0600

    Update: Add find method in item search controller

--- app/controllers/api/v1/items_search_controller.rb
+++ app/controllers/api/v1/items_search_controller.rb
@@ -1,6 +1,29 @@
 class Api::V1::ItemsSearchController < ApplicationController
+
   def find_all
-    items = Item.find_all_items(params[:name])
+    items = Item.find_all(params[:name])
     render json: ItemSerializer.new(items)
   end
+
+  def find
+    if params[:name].present?
+      item = Item.find_one(params[:name])
+    elsif params[:min_price].present?
+      item = Item.find_min_price(params[:min_price])
+      if item
+        render json: ItemSerializer.new(item)
+      elsif item.nil? || item.unit_price < 0
+        render json: { data: { message: 'Error: Item Not Found' } }
+      end
+    elsif params[:max_price].present?
+      item = Item.find_max_price(params[:max_price])
+      if item
+      render json: ItemSerializer.new(item)
+      elsif item.nil?
+        render json: { data: { message: 'Error: Item Not Found' } }
+      end
+    else
+        render status: 400
+      end
+    end
 end
